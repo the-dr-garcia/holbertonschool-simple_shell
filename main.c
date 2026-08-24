d7f1ba65ba1c208c7c96b29c74cb05221b681092
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-08-24 02:00:56 +0000

    Added main.c script

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 on success
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
